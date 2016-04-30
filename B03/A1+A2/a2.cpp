3dae5acc0e6e8380ed1bfae6a0cd75cdf4fc40e5
Alexander Harnisch <alexander.harnisch@tu-dortmund.de>
2016-04-30 23:54:41 +0200

    Blatt 03: 	Aufgaben 1 und 2 fertig programmiert erstmal ohne Animation. Für die Fußball Aufgabe müsste 	rungkutt.cpp noch etwas angepasst werden (Geschwindigkeitsabhängigkeit der Kraft). rungkutt.cpp 	Zeile 52: Ich habe absolut keine Ahnung wieso, damit richtige Werte raus kommen, da nicht 	der Faktor 1/6 stehen darf. Hatte ich nämlich vorher so und habe Stunden gebraucht bis ich 	den "Fehler" gefunden habe... (weil ich ihn bis jetzt nicht verstehe)

---
+++ B03/A1+A2/a2.cpp
@@ -0,0 +1,128 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <functional>
+#include <vector>
+#include "rungkutt.h"
+
+using namespace std;
+
+void F(vector<double> &r, vector<double> &force);
+/* Kraftfeld des harmonischen Oszillators
+ *      r: Ortsvektor (input)
+ *      force: Kraft am Ort r (output)
+ */
+
+void energy(function<void(vector<double> &r, vector<double> &force)> F, vector<vector<double>> &r, vector<vector<double>> &v, vector<double> &U, vector<double> &T);
+/* Berechnet kinetische Energie T und potentielle energie U *      F: Kraftfeld
+ *      r: Ortsvektoren
+ *      v: Geschwindigkeitsvektoren
+ *      U: Potentielle Energie an Orten r mit v (output)
+ *      T: Kinetische Energie an Orten r mit v (output)
+ */
+
+void savedat(string dest, double h, vector<vector<double>> &r, vector<vector<double>> &v, vector<double> &U, vector<double> &T);
+/* Speichermethode
+ */
+
+void doWork(double h, int Ts, vector<double> r_0, vector<double> v_0, string fname);
+/* Führt einen Durchgang mit gegebenen Parametern durch und speichert das
+ * Ergebnis.
+ *      h: Schrittweite
+ *      Ts: Anzahl an zu berechnenden Perioden
+ *      r_0: Startwert für den Ort
+ *      v_0: Startwert für die Geschwindigkeit
+ *      fnmae: Name der Zieldatei
+ */
+
+int main()
+{
+    // Zuerst verschiedene Schrittweiten in 1D testen
+    double h = 1;
+    //while ( h > 1e-5 )
+    //{
+        //stringstream ss;
+        //ss << h;
+        //cout << ss.str() << " ..." << endl;
+        //doWork(h, 5, {1}, {0}, "h="+ss.str()+".dat");
+        //h *= 0.1;
+    //}
+
+    // 3D v_0 = 0
+    h = 1e-4;
+    doWork(h, 2, {1, 1, 1}, {0, 0, 0}, "v_0=0.dat");
+
+    // v nicht parallel zu r
+    doWork(h, 2, {1, 0} , {0, 1}, "v_senkrecht_r.dat");
+    return 0;
+}
+
+void F(vector<double>& r, vector<double>& force)
+{
+    //force.resize(r.size());
+    for ( unsigned i = 0; i < r.size(); i++ )
+    {
+        force[i] = - r[i];
+    }
+}
+
+void energy(function<void(vector<double> &r, vector<double> &force)> F, vector<vector<double>> &r, vector<vector<double>> &v, vector<double> &U, vector<double> &T)
+{
+    double s = r.size();
+    double d = r[0].size();
+    U.resize(s, 0);
+    T.resize(s, 0);
+    vector<double> force(d, 0);
+
+    //#pragma omp parallel for
+    for ( int t = 0; t < s; t++ )
+    {
+       F(r[t], force);
+
+       for ( int i = 0; i < d; i++  )
+       {
+           U[t] += 0.5*r[t][i]*r[t][i];
+           T[t] += 0.5*v[t][i]*v[t][i];
+       }
+    }
+}
+
+void savedat(string dest, double h, vector<vector<double>> &r, vector<vector<double>> &v, vector<double> &U, vector<double> &T)
+{
+    ofstream out;
+    out.open(dest);
+    out.precision(10); // gespeicherte Nachkommastellen ehöhen!!! (Zu viel ist aber auch nicht gut sonst braucht python ewig)
+
+    for ( unsigned t = 0; t < r.size(); t++ )
+    {
+        out << t*h << "\t";
+        for ( unsigned i = 0; i <  r[0].size(); i++ )
+        {
+            out << r[t][i] << "\t";
+        }
+        for ( unsigned i = 0; i <  r[0].size(); i++ )
+        {
+            out << v[t][i];
+                out << "\t";
+        }
+        out << U[t] << "\t" << T[t] << endl;
+    }
+}
+
+void doWork(double h, int Ts, vector<double> r_0, vector<double> v_0, string fname)
+{
+    vector<double> U, T;
+    vector<vector<double>> r, v;
+    // Anfangsbedingungen setzen
+    r.push_back(r_0);
+    v.push_back(v_0);
+
+    // Runge Kutter 4. Ordnung durchführen
+    rungkutt(F, 0, Ts*2*3.14159265, h, r, v);
+
+    // Energie berechnen
+    energy(F, r, v, U, T);
+
+    // Ergebnis speichern
+    savedat(fname, h, r, v, U, T);
+}
