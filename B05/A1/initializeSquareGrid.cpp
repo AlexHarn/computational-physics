0aa87a5c9c85924f27105d259ff5880b73ea05b9
Alex Knodel@BaroperStr <alexander.knodel@tu-dortmund.de>
2016-05-16 19:44:26 +0200

    Init für das quadr Gitter geschrieben. Makefile dazu. Bin an einer Sache kleben geblieben: Ich habe eine Matrix particleinfo, in der ich pro Spalte die 2D Kords für jedes Teilchen abspeichere. Kann man den ganzen Vektor in diese Spalte schreiben? Hab noch nicht herausgefunden wie.. Keine Garantie, dass das funktioniert; Kanns ja noch nicht testen..

---
+++ B05/A1/initializeSquareGrid.cpp
@@ -0,0 +1,62 @@
+#include "../../Eigen/Eigen/Dense" //Eigen liegt oberhalb der B**-Ordner
+#include <math.h>
+
+using namespace Eigen;
+
+#ifndef SquareGrid_H
+#define SquareGrid_H
+class QuadrGitter {  
+ 
+    public:
+        //Methoden
+        void ErstelleQuadrGitter(); //int AnzTeilchen, double dichte
+};
+#endif
+
+void ErstelleQuadrGitter() { //int AnzTeilchen, double dichte)
+    int AnzTeilchen = 100;
+    double dichte = 3;
+    //int AnzTeilchen Anzahl an Teilchen für das Gitter
+    //double dichte Dichte der Partikel
+    MatrixXd particleinfo(2,AnzTeilchen); //2xAnzTeilchen Array mit einem Ortsvektor pro Spalte
+    for(int j = 0; j < AnzTeilchen; j++) { //Anz-Teil-Spalten in der Matrix
+        for(int i = 0; i < 2; i++) { //durch Beginn bei 0 passt die Grenze wieder: 2dim Vektor
+            particleinfo(i,j) = 0;        //füllt mit Nullen
+        }
+    }
+    // Größe der Box (RB: Volumen, bzw. hier Fläche, da 2D); Teilchendichte umgeformt, auf Dim der Länge
+    double L = pow((AnzTeilchen/dichte),0.5);
+    
+    // Finde das kleinste perfekte Quadrat größer oder gleich der Anz an Partikeln
+    // Bietet Gitterplätze für die Teile
+    double AnzQuadrat = 2; //Seite des perfekten Quadrates
+    
+    while (pow(AnzQuadrat,2) < AnzTeilchen)
+    {  
+       //AnzQuadrat += 1.AnzQuadrat; 
+        AnzQuadrat = AnzQuadrat + 1;
+    }
+
+    // 2D Index zum Zählen der Plätze, Startposition untere linke Kante
+    Vector2d aktPos;
+    aktPos << 0, 0;
+    Vector2d Schrittweite;
+    Schrittweite << 0.5, 0.5;
+
+    for(int Teilchen = 1; Teilchen<AnzTeilchen ;Teilchen++)
+    { 
+        // Kord setzen
+        //Addition ist in Eigen etwas schräg. aktPos + (0.5,0.5) fkt nicht
+        particleinfo(:,Teilchen) = (aktPos + Schrittweite)*(L/AnzQuadrat); //Teilchenpos in der Matrix setzen auf jew Kante
+        //Doppelpunkt gibt komplette Zeile,bzw hier Spalte aus (also (x,y) des Teilchens).
+
+        // Rastern des Pointers
+        aktPos(0) = aktPos(0) + 1;
+        if ( aktPos(0) == AnzQuadrat )
+        {
+            aktPos(0) = 0;
+            aktPos(1) = aktPos(1) + 1;
+        }
+            
+    }
+}    
