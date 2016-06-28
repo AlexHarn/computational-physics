b93838920c60ff68ede51ae5c07dfed54ad210a3
Sebastian Bange <sebastianbange94@gmail.com>
2016-06-28 19:32:47 +0200

    B10 A1: 	Aufgabe c jetzt soweit zufriedenstellend fertig

--- B10/A1/main.cpp
+++ B10/A1/main.cpp
@@ -45,7 +45,7 @@ void simulate(vector<double> tp, double tau, double alpha, int N)
 {
     vector<double> theta = ransin(N), magnetization(tp.size(), 0.0), phi1(tp.size(), 0.0), phi2(tp.size(), 0.0);
     // theta: startwerte, magnetization: naja.. formel 6, phi1: phi(0, tp) für alle tp's speichern, phi2: siehe phi1
-    double t, intSize, omega;
+    double t, intSize = tLife(tau), omega;
     // t: Zeit, intSize: Größe des Intervalls bzw Lebensdauer, omega: Formel 1
     int count, count2;
     // count: Zählt für phi1 in welchem Interval man sich befindet, count2: Zählt für phi2
@@ -53,6 +53,7 @@ void simulate(vector<double> tp, double tau, double alpha, int N)
     mt19937 mt(rd());
     uniform_real_distribution<double> dist(0, 2*M_PI);
     // gleichverteilte Zufallszahl für die Berechnung von dem neuen Theta
+    
     
     #pragma omp parallel for
     for ( int i = 0; i < N; i++) // alle Teilchen durchgehen
@@ -65,16 +66,18 @@ void simulate(vector<double> tp, double tau, double alpha, int N)
         
         do
         {
-            omega = 0.5*( 3 * pow(cos(theta[i]), 2) - 1 );
+            omega = 0.5*( 3 * pow(cos(theta[i]), 2) - 1 ); // brauch man noch häufiger
+            
+            // phi(0, tp) berechnen [phi1 genannt]
             if (t < tp.back()-intSize)
             {
                 if ( t > tp[count] ) // True --> ins nächste Interval gehen
                 {
                     phi1[count] += omega*(intSize - ( t - tp[count] )); // Restteil berechnen
-                    while ( (t > tp[count]) && (count < tp.size()-1) )
+                    while ( (t > tp[count]) && (count < tp.size()-1) ) // solange count vergrößern, bis der nächste Messpunkt größer als das aktuelle t ist
                     {
-                        phi1[count+1] = phi1[count];
-                        if ( t > tp[count+1] )
+                        phi1[count+1] = phi1[count]; // Intervall (0, tp[count+1]) hat das Intervall (0, tp[count]) als Teilintervall
+                        if ( t > tp[count+1] ) // Restteil berechnen (siehe oben)
                         {
                             phi1[count+1] += omega*(tp[count+1] - tp[count] );
                         }
@@ -85,26 +88,27 @@ void simulate(vector<double> tp, double tau, double alpha, int N)
                         count++;
                     }
                 }
-                else
+                else // wenn t < tp[count] ist --> phi einfach integrieren
                 {
                     phi1[count] += omega*intSize;
                 }
             }
-                
+            
+            // phi(tp, 2tp) berechnen [phi2 genannt]
             if ( (t > tp[count2]) && (t < 2*tp[count2]) )
             {
-                if ( t - intSize > tp[count2] )
+                if ( t - intSize > tp[count2] ) // testen ob ich das erste mal größer als tp bin
                 {
                     phi2[count2] += omega*intSize;
                 }
-                else
+                else // wenn ja, dann hier rein --> nur Teilintervall integrieren
                 {
                     phi2[count2] = omega*( t - tp[count2] );
                 }
             }
-            else if ( t > tp[count2] )
+            else if ( t > tp[count2] ) // True, wenn t ebenfalls größer als 2tp ist --> das phi2 ist fertig, nächstes phi2 berechnen
             {
-                phi2[count2] += omega*(intSize - ( t - 2*tp[count2] ));
+                phi2[count2] += omega*(intSize - ( t - 2*tp[count2] )); // Restteil, alles analog wie bei phi1
                 while ( t > tp[count2] && (count2 < tp.size()-1) )
                 {
                     count2++;
@@ -120,15 +124,14 @@ void simulate(vector<double> tp, double tau, double alpha, int N)
                 }
             }
 
-            intSize = tLife(tau);
-            t += intSize;
+            // theta springt
             theta[i] = acos( cos(alpha)*cos(theta[i]) - sin(alpha)*sin(theta[i])*cos(dist(mt)) );
-            
-            
-        } while ( t <= 2*tp.back() );
+            t += intSize; // Zeit erhöhen
+            intSize = tLife(tau); // nächstes intSize (Intervall Größe) berechnen
+        } while ( t <= 2*tp.back() ); // <= 2tp.back(), da für phi2 auch bis 2tp integriert werden muss
         for ( int j = 0; j < tp.size(); j++)
         {
-            magnetization[j] += cos(phi1[j] - phi2[j]);
+            magnetization[j] += cos(phi1[j] - phi2[j]); // Mittelung der Magnetisierung über alle Teilchen (1/N kommt bei der Ausgabe)
         }
     }
     
@@ -164,9 +167,8 @@ int main()
     // c)
     vector<double> tp(30);
     for ( int i = 0; i < tp.size(); i++)
-        tp[i] = pow(10, 3.0*i/(tp.size() - 1) - 1);
-    // vector<double> tp(1, 1.0);
+        tp[i] = pow(10, 3.0*i/(tp.size() - 1) - 1); // logarithmische Skalierung der Messpunkte von 10^-1 bis 10^2
     simulate(tp, 0.75, 10*M_PI/180, 1e3);
-    // simulate(tp, 0.05, 10*M_PI/180, 1e3); // das sieht super aus, also scheint der Fehler in einer der nervigen verschachtelten if Abfragen zu liegen.. irgendwo überlappen sich noch die Intervalle..
+
     return 0;
 }
